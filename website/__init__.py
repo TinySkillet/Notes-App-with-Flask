00b4920a3328d7521fbbf621298e40a91db0e68a
Aditya <mirageaditya@gmail.com>
2024-02-01 13:31:19 +0545

    bruh

---
+++ website/__init__.py
@@ -0,0 +1,47 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import path
+from flask_login import LoginManager
+
+
+db = SQLAlchemy()
+DB_NAME = 'database.db'
+
+def create_app():
+    app = Flask(__name__)
+    
+    app.config['SECRET_KEY'] = 'kj13507b08asdf;802/j5lqkjfds`'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{path.join(app.root_path, DB_NAME)}'
+    db.init_app(app)
+    
+    
+    from .auth import auth
+    from .views import views
+    
+    app.register_blueprint(auth, url_prefix='/')
+    app.register_blueprint(views, url_prefix='/')
+    
+    from .models import User, Note
+        
+    create_database(app)
+    
+    
+    # this is telling flask how we load the user
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+    
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
+    
+    return app
+
+
+def create_database(app):
+    if not path.exists('website/' + DB_NAME):
+        with app.app_context():
+            db.create_all()
+        print('Created db succesfully!')
+    else:
+        print('Database already exists')
